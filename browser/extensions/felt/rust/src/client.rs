94d925a6e76e01fbeab69a0270a121ad3899c985
Alexandre Lissy <lissyx+mozillians@lissyx.dyndns.org>
2026-03-05 20:33:53 +0100

    Bug 2021408 - Enterprise: mirror tokens from browser to felt

--- browser/extensions/felt/rust/src/client.rs
+++ browser/extensions/felt/rust/src/client.rs
@@ -87,7 +87,7 @@ impl FeltIpcClient {
         }
     }
 
-    pub fn send_back_tokens(&self) {
+    pub fn send_back_tokens(&self) -> nserror::nsresult {
         trace!("FeltIpcClient::send_back_tokens()");
         match TOKENS.read() {
             Ok(tokens) => {
@@ -98,13 +98,22 @@ impl FeltIpcClient {
                 ));
                 if let Some(tx) = &self.tx {
                     match tx.send(msg) {
-                        Ok(()) => trace!("FeltIpcClient::send_back_tokens() SENT"),
-                        Err(err) => trace!("FeltIpcClient::send_back_tokens() TX ERROR: {}", err),
+                        Ok(()) => {
+                            trace!("FeltIpcClient::send_back_tokens() SENT");
+                            NS_OK
+                        },
+                        Err(err) => {
+                            trace!("FeltIpcClient::send_back_tokens() TX ERROR: {}", err);
+                            NS_ERROR_FAILURE
+                        }
                     }
+                } else {
+                    NS_ERROR_FAILURE
                 }
             }
             Err(_) => {
-                trace!("FeltIpcClient::send_back_tokens failed: couldn't acquire lock",);
+                trace!("FeltIpcClient::send_back_tokens failed: couldn't acquire lock");
+                NS_ERROR_FAILURE
             }
         }
     }
@@ -482,9 +491,9 @@ impl FeltClientThread {
         client.notify_signout();
     }
 
-    pub fn send_back_tokens(&self) {
+    pub fn send_back_tokens(&self) -> nserror::nsresult {
         trace!("FeltClientThread::send_back_tokens()");
         let client = self.ipc_client.borrow();
-        client.send_back_tokens();
+        client.send_back_tokens()
     }
 }

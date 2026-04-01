16d1c2ac812e4c381614c1934851aa7fda5b1a97
Jonathan Mendez <jmendez@mozilla.com>
2026-04-01 17:12:31 -0500

    Fix build: Add AppConstants import to IPPFxaAuthProvider.sys.mjs

--- toolkit/components/ipprotection/fxa/IPPFxaAuthProvider.sys.mjs
+++ toolkit/components/ipprotection/fxa/IPPFxaAuthProvider.sys.mjs
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import { AppConstants } from "resource://gre/modules/AppConstants.sys.mjs";
+
 import { IPPAuthProvider } from "moz-src:///toolkit/components/ipprotection/IPPAuthProvider.sys.mjs";
 
 const lazy = {};

427113a45287885ab539b05df052d62e760fc716
Alexandre Lissy <lissyx+mozillians@lissyx.dyndns.org>
2026-05-06 09:01:52 +0200

    Bug 2022263 - Enterprise: improve testing for FeltErrorReport.sys.mjs
    With new FeltErrorReport.sys.mjs module, update and improve testing capabilities
    to ensure things are still properly tested with the new module. It also
    introduces surfacing more netError error codes to verify the UX is sound
    our Felt side.

---
+++ testing/enterprise/felt_console_error.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import os
+import sys
+
+sys.path.append(os.path.dirname(__file__))
+
+from base_test import Environment
+from felt_tests import FeltTests
+
+
+class FeltConsoleErrorBase(FeltTests):
+    def check_error_bar_message(
+        self,
+        console_addr,
+        selector,
+        expected_heading,
+        error_msg=None,
+        error_msg_contains=None,
+        screenshot_name=None,
+    ):
+        with self._driver.using_prefs(
+            {
+                "enterprise.console.address": console_addr
+                or f"http://localhost:{self.console_port}"
+            },
+            default_branch=True,
+        ):
+            if console_addr:
+                self.submit_email()
+
+            self.assert_error_bar_message(
+                selector=selector,
+                expected_heading=expected_heading,
+                error_msg=error_msg,
+                error_msg_contains=error_msg_contains,
+                screenshot_name=screenshot_name,
+            )
+
+    def assert_error_bar_message(
+        self,
+        selector,
+        expected_heading,
+        error_msg=None,
+        error_msg_contains=None,
+        screenshot_name=None,
+        source=None,
+    ):
+        if not screenshot_name:
+            screenshot_name = self._testMethodName
+
+        self._driver.set_context("chrome")
+
+        self._logger.info(f"Checking selector: {selector}")
+        error = self.get_elem(selector)
+        message = self._wait.until(
+            lambda d: (
+                h.strip()
+                if (h := error.get_attribute("heading"))
+                and expected_heading in h.strip()
+                else False
+            )
+        )
+        assert expected_heading in message, f"Unexpected error message: {message}"
+
+        if source:
+            source_attribute = self._wait.until(
+                lambda d: (
+                    h.strip()
+                    if (h := error.get_attribute("source")) and source in h.strip()
+                    else False
+                )
+            )
+            assert source in source_attribute, (
+                f"Error element is missing the {source} attribute. Found: {source_attribute}"
+            )
+
+        if error_msg is not None:
+            details = self.get_elem(f"{selector} .felt-browser-error-details")
+            details_text = details.get_property("textContent").strip()
+            assert details_text == error_msg, (
+                f"Unexpected error message: '{details_text}' expected: '{error_msg}'"
+            )
+
+        if error_msg_contains is not None:
+            details = self.get_elem(f"{selector} .felt-browser-error-details")
+            details_text = details.get_property("textContent").strip()
+            assert error_msg_contains in details_text, (
+                f"Expected '{error_msg_contains}' in error details: '{details_text}'"
+            )
+
+        self.maybe_save_screenshot(Environment.FELT, screenshot_name)
+
+        self._driver.set_context("content")
---
+++ testing/enterprise/test_felt_verify_neterror.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import os
+import sys
+
+sys.path.append(os.path.dirname(__file__))
+
+from felt_console_error import FeltConsoleErrorBase
+
+
+class FeltVerifyNetError(FeltConsoleErrorBase):
+    def trigger_netError(self, error):
+        self.reload_chrome_window()
+        self._driver.set_context("chrome")
+        self._driver.execute_async_script(
+            """
+            const callback = arguments[arguments.length - 1];
+            const { FeltErrorReport } = ChromeUtils.importESModule("resource:///modules/FeltErrorReport.sys.mjs");
+            FeltErrorReport.handleNetError({errorPageURI: `about:neterror?e=${arguments[0]}&u=https%3A//www.mozilla.org`}).then(_ => callback());
+            """,
+            [error],
+        )
+
+    """
+    This test is here to verify the rendering of about:neterror's error that
+    are mapped into FeltErrorReport ONLY, there is no network involved.
+    """
+
+    def test_felt_neterror_handling(self):
+        # We are not going to start the browser so do not try to close it
+        self._manually_closed_child = True
+        # Make sure the iteration and callable() check below will not choke on
+        # this missing attribute
+        self._child_driver = None
+
+        object_methods = [
+            method_name
+            for method_name in dir(self)
+            if callable(getattr(self, method_name))
+            and method_name.startswith("run_netError")
+        ]
+        for m in object_methods:
+            getattr(self, m)()
+
+    # Generated from errorCode that can be found in toolkit/content/errors/net-errors.mjs:
+    # $ for e in $(grep "errorCode:" toolkit/content/errors/net-errors.mjs | sed -e "s/^.*errorCode: //g" -e "s/,$//g" -e 's/"//g'); do echo -e "def run_netError_$e(self):\n     self.assert_error(\n        \"$e\",\n        \"title\",\n        \"error\",\n    )\n"; done;
+    def run_netError_dnsNotFound(self):
+        self.trigger_netError("dnsNotFound")
+        self.assert_error_bar_message(
+            selector=".felt-browser-error-connection",
+            expected_heading="Server not found",
+            error_msg="Try connecting on a different device. Check your modem or router. Disconnect and reconnect to Wi-Fi.",
+            screenshot_name=f"{self._testMethodName}_dnsNotFound",
+        )
+
+    def run_netError_NS_ERROR_OFFLINE(self):
+        self.trigger_netError("NS_ERROR_OFFLINE")
+        self.assert_error_bar_message(
+            selector=".felt-browser-error-connection",
+            expected_heading="Looks like there’s a problem with your internet connection",
+            error_msg="Try connecting on a different device. Check your modem or router. Disconnect and reconnect to Wi-Fi.",
+            screenshot_name=f"{self._testMethodName}_NS_ERROR_OFFLINE",
+        )
+
+    def run_netError_netTimeout(self):
+        self.trigger_netError("netTimeout")
+        self.assert_error_bar_message(
+            selector=".felt-browser-error-connection",
+            expected_heading="The connection has timed out",
+            error_msg="The server at www.mozilla.org is taking too long to respond.",
+            screenshot_name=f"{self._testMethodName}_netTimeout",
+        )
+
+    def run_netError_blockedByPolicyEnterprise(self):
+        self.trigger_netError("blockedByPolicyEnterprise")
+        self.assert_error_bar_message(
+            selector=".felt-browser-error-connection",
+            expected_heading="Access to this site is restricted",
+            error_msg="If you believe this is an error or need access for work purposes, please contact your IT administrator.",
+            screenshot_name=f"{self._testMethodName}_blockedByPolicyEnterprise",
+        )
+
+    def run_netError_MOZILLA_PKIX_ERROR_MITM_DETECTED(self):
+        self.trigger_netError("MOZILLA_PKIX_ERROR_MITM_DETECTED")
+        self.assert_error_bar_message(
+            selector=".felt-browser-error-connection",
+            expected_heading="Unable to connect. Please contact your administrator.",
+            error_msg="Firefox Enterprise spotted a potentially serious security issue with www.mozilla.org. Someone pretending to be the site could try to steal things like credit card info, passwords, or emails.",
+            screenshot_name=f"{self._testMethodName}_MOZILLA_PKIX_ERROR_MITM_DETECTED",
+        )
+
+    def run_netError_netOffline(self):
+        self.trigger_netError("netOffline")
+        self.assert_error_bar_message(
+            selector=".felt-browser-error-connection",
+            expected_heading="Looks like there’s a problem with your internet connection",
+            error_msg="Try connecting on a different device. Check your modem or router. Disconnect and reconnect to Wi-Fi.",
+            screenshot_name=f"{self._testMethodName}_netOffline",
+        )

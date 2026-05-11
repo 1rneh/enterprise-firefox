cb1ad4a10047b2993b950d711109482127d40a78
lissyx <1645737+lissyx@users.noreply.github.com>
2026-05-11 19:21:33 +0200

    Revert "Bug 2038707 - Enterprise: do not schedule complete kind for level < 3"

--- taskcluster/gecko_taskgraph/util/verify.py
+++ taskcluster/gecko_taskgraph/util/verify.py
@@ -178,22 +178,6 @@ def verify_task_graph_symbol(task, taskgraph, scratch_pad, graph_config, paramet
 
 
 @verifications.add("full_task_graph")
-def verify_task_graph_no_shippable_enterprise_level_not_1(
-    task, taskgraph, scratch_pad, graph_config, parameters
-):
-    if task is None:
-        return
-
-    if int(parameters["level"]) < 3:
-        return
-
-    if "enterprise" in task.label and "shippable" in task.label:
-        raise Exception(
-            f"Enterprise shippable job {task.label} should not be scheduled on level {int(parameters['level'])}"
-        )
-
-
-@verifications.add("full_task_graph")
 def verify_task_graph_symbol_enterprise(
     task, taskgraph, scratch_pad, graph_config, parameters
 ):

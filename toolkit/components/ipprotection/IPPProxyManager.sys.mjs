8abda7d6154afe838b2b639a99e9772308cc770e
Josh Porter <joporter@mozilla.com>
2026-06-03 08:17:02 -0400

    No Bug - Remove duplicate channelFilter method in IPProxyManager

--- toolkit/components/ipprotection/IPPProxyManager.sys.mjs
+++ toolkit/components/ipprotection/IPPProxyManager.sys.mjs
@@ -259,10 +259,6 @@ class IPPProxyManagerSingleton extends EventTarget {
     }
   }
 
-  channelFilter() {
-    return this.#connection;
-  }
-
   cancelChannelFilter() {
     if (this.#connection) {
       this.#connection.stop();

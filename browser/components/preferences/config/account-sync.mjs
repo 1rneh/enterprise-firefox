d567379b151be957a2d49f9245658aeb9794d5b7
Janika <jneuberger@mozilla.com>
2026-06-15 22:41:43 +0200

    Lint changes

--- browser/components/preferences/config/account-sync.mjs
+++ browser/components/preferences/config/account-sync.mjs
@@ -614,9 +614,9 @@ Preferences.addSetting({
   },
   visible: () => {
     return !(
-        AppConstants.MOZ_ENTERPRISE &&
-        !Services.policies.isAllowed("change-sync-state")
-      )
+      AppConstants.MOZ_ENTERPRISE &&
+      !Services.policies.isAllowed("change-sync-state")
+    );
   },
 });
 

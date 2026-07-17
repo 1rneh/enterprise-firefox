c60d7af5b84385196c5a3993047108ae0491ccd8
Alexandre Lissy <lissyx+mozillians@lissyx.dyndns.org>
2026-07-17 06:58:52 +0200

    No Bug - Fix merge mistake

--- taskcluster/gecko_taskgraph/transforms/task.py
+++ taskcluster/gecko_taskgraph/transforms/task.py
@@ -160,8 +160,8 @@ class TaskDescriptionSchema(Schema, kw_only=True):
     # https://firefox-ci-tc.services.mozilla.com/schemas/taskcluster-treeherder/v1/task-treeherder-config.json
     # If not specified, no treeherder extra information or routes will be
     # added to the task
-    treeherder: TOptional[TreeherderSchema] = None
-    treeherder_group: TOptional[str] = None
+    treeherder: Optional[TreeherderSchema] = None
+    treeherder_group: Optional[str] = None
     # information for indexing this build so its artifacts can be discovered;
     # if omitted, the build will not be indexed.
     index: Optional[IndexSchema] = None

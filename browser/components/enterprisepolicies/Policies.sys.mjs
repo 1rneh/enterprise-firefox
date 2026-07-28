fd1d0c8d3fe5e19ea07d39029fef5f662c2fef18
Janika <jneuberger@mozilla.com>
2026-07-28 11:44:23 +0200

    Bug 2056606 - Add live support for policy WebsiteFilter

--- browser/components/enterprisepolicies/Policies.sys.mjs
+++ browser/components/enterprisepolicies/Policies.sys.mjs
@@ -4344,6 +4344,9 @@ export var Policies = {
     onBeforeUIStartup(manager, param) {
       lazy.WebsiteFilter.init(param.Block || [], param.Exceptions || []);
     },
+    onRemove(_manager, _oldParams) {
+      lazy.WebsiteFilter.uninit();
+    },
   },
 
   WindowsSSO: {

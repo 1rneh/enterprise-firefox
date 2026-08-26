98529371d59e964c08441e0be8d4e0ff5fffcac5
Alexandre Lissy <lissyx+mozillians@lissyx.dyndns.org>
2026-08-26 09:46:37 +0200

    Bug 2066673 - Add a try-status-report task to forward from try to Treeherder/Github Checks

--- taskcluster/gecko_taskgraph/target_tasks.py
+++ taskcluster/gecko_taskgraph/target_tasks.py
@@ -633,7 +633,17 @@ def target_tasks_enterprise_firefox_with_tests(
 
         return False
 
-    return [l for l in filtered_for_project if filter(full_task_graph[l])]
+    selected = [l for l in filtered_for_project if filter(full_task_graph[l])]
+
+    level = int(parameters["level"])
+    # Make sure to always schedule, but on PR only
+    selected += [
+        label
+        for label, task in full_task_graph.tasks.items()
+        if task.kind == "try-status" and label not in selected and level < 3
+    ]
+
+    return selected
 
 
 @register_target_task("graphics_tasks")

bbc6de52d0db5db833b8792c943e7b478d5b3190
Janika <jneuberger@mozilla.com>
2026-09-17 16:24:52 +0200

    fix: revert felt window to resizable

--- toolkit/components/felt/Felt.sys.mjs
+++ toolkit/components/felt/Felt.sys.mjs
@@ -395,7 +395,7 @@ export class Felt {
 
   showWindow(errorMessage = "") {
     // Height and width are for now set to fit the sso.mozilla.com without the need to resize the window
-    let flags = "chrome,private,centerscreen,titlebar,width=727,height=744";
+    let flags = "chrome,private,resizable,centerscreen,titlebar,width=727,height=744";
     const queryString = errorMessage
       ? `?error=${encodeURIComponent(errorMessage)}`
       : "";
